122d04fb4f836a2dd23ed9799d60692234e730b1
Izak Mohamed <71826622+IZAK-M@users.noreply.github.com>
2026-07-03 23:15:10 +0200

    Exercice 1

---
+++ POO/pratique.py
@@ -0,0 +1,35 @@
+class Compte:
+    def __init__(self, titulaire: str, num_compte: str, solde: float = 0):
+        self.titulaire = titulaire
+        self.num_compte = num_compte
+        self.solde = solde
+        self.historique = []
+
+    def deposer(self, montant: float):
+        if not isinstance(montant,(int, float)):
+            raise TypeError("Le montant doit être un nombre")
+        elif montant <= 0:
+            raise ValueError("Le montant doit être strictement positif")
+        self.solde += montant
+        self.historique.append({'type': 'dépôt', 'montant': montant, 'solde_apres': self.solde})
+
+    def retirer(self, montant: float):
+        if not isinstance(montant,(int, float)):
+            raise TypeError("Le montant doit être un nombre")
+        if self.solde <= 0:
+            raise ValueError("Le montant doit être strictement positif")
+        if self.solde < montant:
+            raise ValueError(f"Solde insuffisant - disponible : {self.solde} €")
+        self.solde -= montant
+
+        self.historique.append({'type': 'Retrait', 'montant': montant, 'solde_apres':self.solde})
+
+    def afficher_solde(self):
+        print(f"Compte {self.num_compte} — {self.titulaire} : {self.solde} €")
+
+    def afficher_historique(self):
+        print(f"--- Historique du compte {self.num_compte} ---")
+        for i , e in enumerate(self.historique, start=1):
+            signe = "+" if e["type"] == "dépôt" else "-"
+            print(f"{i}. {e['type']}   {signe}{e['montant']} -> solde : {e['solde_apres']}")
+
