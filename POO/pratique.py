bb993aad77685c5076672e5e7ba93f2d69460963
Izak Mohamed <71826622+IZAK-M@users.noreply.github.com>
2026-07-04 23:06:04 +0200

    feat : ajout de code a ma classe Compte

--- POO/pratique.py
+++ POO/pratique.py
@@ -1,14 +1,17 @@
 class Compte:
+    nombre_comptes = 0
+    taux_interet = 0.02
     def __init__(self, titulaire: str, num_compte: str, solde: float = 0):
         self.titulaire = titulaire
         self.num_compte = num_compte
         self.solde = solde
         self.historique = []
+        Compte.nombre_comptes += 1
 
     def deposer(self, montant: float):
         if not isinstance(montant,(int, float)):
             raise TypeError("Le montant doit être un nombre")
-        elif montant <= 0:
+        if montant <= 0:
             raise ValueError("Le montant doit être strictement positif")
         self.solde += montant
         self.historique.append({'type': 'dépôt', 'montant': montant, 'solde_apres': self.solde})
@@ -22,7 +25,7 @@ class Compte:
             raise ValueError(f"Solde insuffisant - disponible : {self.solde} €")
         self.solde -= montant
 
-        self.historique.append({'type': 'Retrait', 'montant': montant, 'solde_apres':self.solde})
+        self.historique.append({'type': 'retrait', 'montant': montant, 'solde_apres':self.solde})
 
     def afficher_solde(self):
         print(f"Compte {self.num_compte} — {self.titulaire} : {self.solde} €")
@@ -30,6 +33,16 @@ class Compte:
     def afficher_historique(self):
         print(f"--- Historique du compte {self.num_compte} ---")
         for i , e in enumerate(self.historique, start=1):
-            signe = "+" if e["type"] == "dépôt" else "-"
+            if e["type"] == "dépôt":
+                signe = "+"
+            elif e["type"] == "retrait":
+                signe = "-"
+            else:
+                signe = "*"
             print(f"{i}. {e['type']}   {signe}{e['montant']} -> solde : {e['solde_apres']}")
+
+    def appliquer_interets(self):
+        interets = round(self.solde * Compte.taux_interet, 2)
+        self.solde += interets
+        self.historique.append({'type': 'interet', 'montant': interets, 'solde_apres': self.solde})
 
